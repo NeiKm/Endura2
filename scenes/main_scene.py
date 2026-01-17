4fbe9e85e5566236cbde25102c7a81d073f37dec
Kirill(Nei_km) <kirill.kim.0223@gmail.com>
2026-01-17 23:16:09 +0900

    Добавлены статические ресурсы и облегчена тестовая сцена

--- scenes/main_scene.py
+++ scenes/main_scene.py
@@ -8,13 +8,29 @@ class FlatWorld(Entity):
         super().__init__()
         floor_size = 64
         spacing = 1
-        world = factory.create_entity(
-            model='vr_modern_gallery_room (1).glb',
-            scale=1.5,
-            position=(0, 0, 0),
-            double_sided=True,
-            collider='box'
+        self.test_platform = factory.create_entity(
+            model="cube",
+            texture = "grass",
+            scale = (floor_size, 1, floor_size),
+            position = (0, -2, 0),
+            parent = self
         )
+        self.test_cube = factory.create_entity(
+            model = "cube",
+            texture = "white_cube",
+            scale = (1, 1, 1),
+            position =  (0, 0, 0),
+            color = color.gray,
+            parent = self
+        )
+        # world = factory.create_entity(
+        #     model='vr_modern_gallery_room (1).glb',
+        #     scale=1.5,
+        #     position=(0, 0, 0),
+        #     double_sided=True,
+        #     collider='box'
+        # )
+        
 
 class MainScene(Entity):
     def __init__(self):
@@ -30,3 +46,6 @@ class MainScene(Entity):
     def input(self, key):
         if key == "escape":
             application.quit()
+
+    def update(self):
+        self.world.test_cube.rotation += Vec3(30, 60, 0) * time.dt
