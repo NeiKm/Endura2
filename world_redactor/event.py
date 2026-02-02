2cb073b14705099cea97aca590062d5de32f57dc
botsusDJ <davidjibuti6@gmail.com>
2026-02-02 19:32:06 +0400

    добавлена команда копирования, а также оброботчик ошибок

--- world_redactor/event.py
+++ world_redactor/event.py
@@ -1,12 +1,9 @@
 from ursina import *
+from entity import EntityManager
 
 class Event:
-    
     def __init__(self, scene):
         self.scene = scene
-
-    def delete_entity(self, name):
-        destroy(self.scene.get_entity(name))
 
 
 class CommandHandler:
@@ -53,15 +50,30 @@ class CommandHandler:
                 pass
 
             case "delete":
-                pass
+                destroy(self.ent_manager.selected_block)
 
             case "copy":
-                pass
+                self.copy_selected_entity()
 
             case _:
-                print(f"Неизвестная кмда {command}")
+                print(f"Неизвестная команда {command}")
 
     def move_entity(self, pos: Vec3):
-        print(self.ent_manager.selected_block)
         if self.ent_manager.selected_block:
             self.ent_manager.selected_block.position = pos
+
+    def copy_entity(self):
+        sel_entity = self.ent_manager.selected_entity
+
+        new_entity = self.ent_manager.create_entity(
+            pos=sel_entity.position,
+            scale=sel_entity.scale,
+            rotation=sel_entity.rotation,
+            texture=sel_entity.texture,
+            texture_scale=sel_entity.texture_scale,
+            color=sel_entity.color,
+            collider=sel_entity.collider
+        )
+        new_entity.y += sel_entity.scale.y
+
+        self.ent_manager.selected_entity = new_entity
